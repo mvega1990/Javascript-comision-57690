47f8cebde9bbe4db5b0832b651801333f560197c
MaximilianoVega <maximiliano.vega.zg@gmail.com>
2024-05-19 16:51:43 -0300

    Agrego class constructora, array y function find de productos y filter por precio a gastar

--- main.js
+++ main.js
@@ -1,48 +1,79 @@
+const iva = 1.21;
+const descuentoMayorista = 0.2;
 
-
-let producto1 = "martillo"
-let producto2 = "pinza"
-let producto3 = "destornillador"
-let producto4 = "taladro"
-const iva = 1.21
-let precioMartillo = 1000
-let precioPinza = 1200
-let precioDestornillador = 1500
-let precioTaladro = 2000
-let descuentoMayorista = 0.2
-
-function precioTotal(precio, iva, cantidad) {
-   return (precio * iva) * cantidad
+class Producto {
+    constructor(id, nombre, marca, precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.marca = marca;
+        this.precio = precio;
+    }
+    sumaIva() {
+        return this.precio = this.precio * iva;
+    }
+    precioTotalMayorista(precio, descuento, iva, cantidad) {
+        return ((precio - (precio * descuento)) * iva) * cantidad;
+    }
 }
 
-function precioTotalMayorista(precio, descuento, iva, cantidad) {
-   return ((precio - (precio * descuento)) * iva) * cantidad
+let productos = [];
+
+let producto1 = new Producto("1", "amoladora", "umi", 80000);
+let producto2 = new Producto("2", "taladro", "lusqtoff", 70000);
+let producto3 = new Producto("3", "pistola de calor", "gamma", 30000);
+let producto4 = new Producto("4", "destornillador", "bremen", 10000);
+let producto5 = new Producto("5", "alicate", "bremen", 8000);
+let producto6 = new Producto("6", "pinza", "bremen", 6000);
+
+productos.push(producto1, producto2, producto3, producto4, producto5, producto6);
+
+console.table(productos);
+
+
+function encontrarProducto() {
+    let palabraClave = prompt("¿Qué producto estás buscando?").trim().toLowerCase();
+    if (!isNaN(palabraClave) || palabraClave ===""){
+        alert("ingrese datos validos")
+        return null
+        
+    }
+    let resultado = productos.find((producto) => producto.nombre.toLowerCase().includes(palabraClave));
+
+    if (resultado) {
+        alert("Tenemos ese producto")
+        return resultado;
+    } else {
+        alert("No tenemos ese producto");
+        return null;
+    }
 }
-alert("Tenemos estos productos: Martillo, pinzas, destornillador y taladro")
-let iterar = confirm("Queres comprar algun producto?")
-while (iterar) {
-   let producto = prompt("Ingrese el producto que quiere comprar").toLowerCase();
-   let cantidad = parseInt(prompt("Cuantos necesita?\nSi son 5 o mas tenes un 20% de desc"))
-   if (producto == "" || cantidad === "" || isNaN(cantidad)) {
-      alert("ingresa datos validos")
-   } else if (producto == producto1 && cantidad >= 5) {
-      alert("El total incluido de la compra con iva es de " + "$"+ precioTotalMayorista(precioMartillo, descuentoMayorista, iva, cantidad))
-   } else if (producto == producto2 && cantidad >= 5) {
-      alert("El total incluido de la compra con iva es de " + "$"+ precioTotalMayorista(precioPinza, descuentoMayorista, iva, cantidad))
-   } else if (producto == producto3 && cantidad >= 5) {
-      alert("El total incluido de la compra con iva es de " +"$"+ precioTotalMayorista(precioDestornillador, descuentoMayorista, iva, cantidad))
-   } else if (producto == producto4 && cantidad >= 5) {
-      alert("El total incluido  de la compra con iva es de " +"$"+ precioTotalMayorista(precioTaladro, descuentoMayorista, iva, cantidad))
-   } else if (producto == producto1 && cantidad < 5) {
-      alert("El total incluido de la compra con iva es de " +"$"+ precioTotal(precioMartillo, iva, cantidad))
-   } else if (producto == producto2 && cantidad < 5) {
-      alert("El total incluido de la compra con iva es de " +"$"+ precioTotal(precioPinza, iva, cantidad))
-   } else if (producto == producto3 && cantidad < 5) {
-      alert("El total incluido de la compra con iva es de " +"$"+ precioTotal(precioDestornillador, iva, cantidad))
-   } else if (producto == producto4 && cantidad < 5) {
-      alert("El total incluido de la compra con iva es de " +"$"+ precioTotal(precioTaladro, iva, cantidad))
-   } else {
-      alert("no tenemos ese producto")
-   }
-   iterar = confirm("queres saber el precio de otra compra?")
+function filtrarProductosSegunPrecio(){
+    let palabraClave= parseInt(prompt("Ingrese el valor maximo que quiere gastar"))
+    if (isNaN(palabraClave) || palabraClave=="") {
+      alert("Por favor, ingrese un número válido.");
+      return;
+  }
+    let resultado = productos.filter(producto => producto.precio <= palabraClave)
+    if(resultado.length>0){
+        console.table(resultado);
+    }else{
+        alert ("No hay productos menores a ese precio");
+    }
 }
+
+let iterar=true
+
+    while (iterar) {
+        let resultado = encontrarProducto();
+        if (resultado) {
+            iterar = confirm("¿Quieres comprar el producto?");
+        let cantidad = parseInt(prompt("¿Cuántos necesita?\nSi son 5 o más, tienes un 20% de descuento"));
+        if (cantidad >= 5) {
+            alert("El valor de su compra es de " + resultado.precioTotalMayorista(resultado.precio, descuentoMayorista, iva, cantidad));
+        } else {
+            alert("El valor de su compra es de " + (resultado.precio * cantidad * iva));
+        }
+        iterar = confirm("¿Desea continuar?");
+    }
+}
+filtrarProductosSegunPrecio()
